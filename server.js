f6c61ca357a984308735d58d7ec631d7cf5bbd81
Michael Sweeney <msweene4@uwo.ca>
2017-11-03 16:46:56 +0000

    P1 first

--- server.js
+++ server.js
@@ -15,9 +15,21 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+
+var Bear     = require('./app/models/bear');
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
